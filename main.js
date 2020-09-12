608bad7b82ff842c4fc07d197ab8441de28d5930
Ghidyon <gideonakunana@gmail.com>
2020-09-12 19:57:25 +0100

    modified functions

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const input = document.querySelector('#input');
+
 const button0 = () => input.value += 0;
-const point = () => input.value += ".";
-const button00 = () => input.value += "00";
-const button1 = () => input.value += 1;
+const button1 = () => {
+  forNumbers(3);
+};
 const button2 = () => input.value += 2;
 const button3 = () => input.value += 3;
 const button4 = () => input.value += 4;
@@ -11,43 +12,81 @@ const button6 = () => input.value += 6;
 const button7 = () => input.value += 7;
 const button8 = () => input.value += 8;
 const button9 = () => input.value += 9;
+const clearAll = () => input.value = "";
+
+const forNumbers = (num) => {
+  if (input.value === "0" || Math.max(input.value) === 0) {
+    input.value = num;
+  }
+  else {
+    input.value += num;
+  }
+}
+
+const button00 = () => {
+  if (input.value === "") {
+    input.value += "0";
+  }
+  else {
+    input.value += "00";
+  }
+}
+
+const point = () => {
+  let lastInput = input.value.charAt(input.value.length - 1);
+  if (input.value === "") {
+    input.value += "0.";
+  }
+  else if (lastInput === ".") {
+    input.value += "";
+  }
+  else {
+    input.value += ".";
+  }
+}
 
 const divide = () => {
-  let lastInput = input.value.length - 1;
-  if (input.value === '' || input.value.charAt(lastInput) === "\u00f7" || input.value.charAt(lastInput) === "\u00d7" || input.value.charAt(lastInput) === "+" || input.value.charAt(lastInput) === "-") { }
+  let lastInput = input.value.charAt(input.value.length - 1);
+  if (input.value === '' || lastInput === "\u00f7" || lastInput === "\u00d7" || lastInput === "+" || lastInput === "-") {
+    input.value += "";
+  }
   else {
     input.value += "\u00f7";
   }
 }
 
 const multiply = () => {
-  let lastInput = input.value.length - 1;
-  if (input.value === '' || input.value.charAt(lastInput) == "\u00d7" || input.value.charAt(lastInput) === "\u00f7" || input.value.charAt(lastInput) === "+" || input.value.charAt(lastInput) === "-") { }
+  let lastInput = input.value.charAt(input.value.length - 1);
+  if (input.value === '' || lastInput === "\u00d7" || lastInput === "\u00f7" || lastInput === "+" || lastInput === "-") {
+    input.value += "";
+  }
   else {
     input.value += "\u00d7";
   }
 }
 
 const minus = () => {
-  let lastInput = input.value.length - 1;
-  if (input.value.charAt(lastInput) == "-" || input.value.charAt(lastInput) === "+") { }
+  let lastInput = input.value.charAt(input.value.length - 1);
+  if (lastInput == "-" || lastInput === "+") {
+    input.value += "";
+  }
   else {
     input.value += "-";
   }
 }
 
 const add = () => {
-  let lastInput = input.value.length - 1;
-  if (input.value === '' || input.value.charAt(lastInput) == "+") { }
+  let lastInput = input.value.charAt(input.value.length - 1);
+  if (input.value === '' || lastInput == "+" || lastInput === "\u00f7" || lastInput === "\u00d7" || lastInput == "-") {
+    input.value += "";
+  }
   else {
     input.value += "+";
   }
 }
 
-const clearAll = () => input.value = "";
-
 const equals = () => {
-  let evaluate = input.value.replace(/\u00f7/gi, "/");
-  evaluate = evaluate.replace(/\u00d7/gi, "*");
-  input.value = eval(evaluate);
-};
+  let calculate = input.value.replace(/\u00f7/gi, "/");
+  calculate = calculate.replace(/\u00d7/gi, "*");
+  input.value = eval(calculate);
+}
