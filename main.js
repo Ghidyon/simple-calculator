dbe3e193ef4846477bf2a17888233895d02d9197
Ghidyon <gideonakunana@gmail.com>
2020-09-12 18:27:58 +0100

    seventh commit

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const input = document.querySelector('#input');
 const button0 = () => input.value += 0;
 const point = () => input.value += ".";
-const button00 = () => input.value += 00;
+const button00 = () => input.value += "00";
 const button1 = () => input.value += 1;
 const button2 = () => input.value += 2;
 const button3 = () => input.value += 3;
@@ -11,21 +11,43 @@ const button6 = () => input.value += 6;
 const button7 = () => input.value += 7;
 const button8 = () => input.value += 8;
 const button9 = () => input.value += 9;
+
 const divide = () => {
-    if (input.value === '') {
-     console.log("let's fixed this");
-    } else {
-     input.value += "/" //"\u00f7";
-   }
+  let lastInput = input.value.length - 1;
+  if (input.value === '' || input.value.charAt(lastInput) === "\u00f7" || input.value.charAt(lastInput) === "\u00d7" || input.value.charAt(lastInput) === "+" || input.value.charAt(lastInput) === "-") { }
+  else {
+    input.value += "\u00f7";
+  }
 }
 
-const multiply = () => input.value += "*";//"\u00d7";
-const minus = () => input.value += "-";
-const add = () => input.value += "+";
+const multiply = () => {
+  let lastInput = input.value.length - 1;
+  if (input.value === '' || input.value.charAt(lastInput) == "\u00d7" || input.value.charAt(lastInput) === "\u00f7" || input.value.charAt(lastInput) === "+" || input.value.charAt(lastInput) === "-") { }
+  else {
+    input.value += "\u00d7";
+  }
+}
+
+const minus = () => {
+  let lastInput = input.value.length - 1;
+  if (input.value.charAt(lastInput) == "-" || input.value.charAt(lastInput) === "+") { }
+  else {
+    input.value += "-";
+  }
+}
+
+const add = () => {
+  let lastInput = input.value.length - 1;
+  if (input.value === '' || input.value.charAt(lastInput) == "+") { }
+  else {
+    input.value += "+";
+  }
+}
+
 const clearAll = () => input.value = "";
 
-
 const equals = () => {
-  input.value = eval(input.value)
-  
+  let evaluate = input.value.replace(/\u00f7/gi, "/");
+  evaluate = evaluate.replace(/\u00d7/gi, "*");
+  input.value = eval(evaluate);
 };
